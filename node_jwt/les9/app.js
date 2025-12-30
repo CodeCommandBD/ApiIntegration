b1472b51b7a4d860e99d8e395c403917ded78850
shanto <shantokumar22@gmail.com>
2025-12-30 23:23:44 +0600

    Initialize Express app with body-parser and a `/user` POST endpoint.

--- node_jwt/les9/app.js
+++ node_jwt/les9/app.js
@@ -1,35 +1,24 @@
 const express = require("express");
 const app = express();
-const userRouter = require("./router/user.router");
-const path = require("path");
+const bodyParser = require('body-parser')
 
 
-// user all router
-app.use('/user',userRouter)
+
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
 
 
-// register router
-app.use('/register', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'register.html'))
 
-})
 
-// login router
-app.get('/login', (req, res) =>{
-   res.cookie('name', 'shanto')
-   res.cookie('age', 25)
-   res.end()
-})
 
 // home page
-app.use('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'index.html'))
+app.post('/user', (req, res) =>{
+    const name = req.body.name
+    const age = req.body.age
+    res.send(`User ${name} ${age}`)
 })
 
-// user ke error bujanor jonno  not found page
-app.use((req, res) =>{
-    res.sendFile(path.join(__dirname, '404.html'))
-})
+
 
 
 module.exports = app;
