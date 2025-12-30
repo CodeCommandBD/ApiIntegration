39d585731eb710f21ef5debcacc5e26474224204
shanto <shantokumar22@gmail.com>
2025-12-30 13:35:40 +0600

    add basic HTTP server serving random fruit names

--- node_jwt/les5/index.js
+++ node_jwt/les5/index.js
@@ -1,8 +1,12 @@
+// use npm i random-fruits-name
+// extarnal module
 const http = require("http");
+const getRandomFruitsName = require('random-fruits-name')
+
 const port = 8080;
 
 const server = http.createServer((req, res)=>{
-    res.end("Hello World")
+    res.end(getRandomFruitsName())
 });
 
 
