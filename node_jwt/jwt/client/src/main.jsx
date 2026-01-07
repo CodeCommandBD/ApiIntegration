a72e7a5e874db2065804620e73ac65bf388b13f7
shanto <shantokumar22@gmail.com>
2026-01-08 04:24:04 +0600

    update

--- node_jwt/jwt/client/src/main.jsx
+++ node_jwt/jwt/client/src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router-dom";
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
+import Profile from './components/profile.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      }
+    ],
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
