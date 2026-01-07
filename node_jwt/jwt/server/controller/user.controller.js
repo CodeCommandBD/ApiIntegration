8265eccc028bc86275b7607b279b09d89c57b94a
shanto <shantokumar22@gmail.com>
2026-01-08 02:45:03 +0600

    update

--- node_jwt/jwt/server/controller/user.controller.js
+++ node_jwt/jwt/server/controller/user.controller.js
@@ -1,11 +1,66 @@
 const path = require("path");
+const userModel = require("../model/user.model");
+
 
 // register controller get
 const registerControllerGet = (req, res) => {
-   res.sendFile(path.join(__dirname, "../views", "register.html"));
+  res.sendFile(path.join(__dirname, "../views", "register.html"));
 };
 
+// register controller post
+const registerControllerPost = async (req, res) => {
+  try {
+    // check user is already registered or not
+    const user = await userModel.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+
+    // create new user
+    const newUser = new userModel({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await newUser
+      .save()
+      .then((user) => {
+        res.send({
+          success: true,
+          message: "User registered successfully!",
+          user: {
+            username: user.username,
+            email: user.email,
+            id: user._id,
+          },
+        });
+      })
+      .catch((error) => {
+        res.send({
+          success: false,
+          message: "User registration failed!",
+          error: error,
+        });
+      });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
+
+// login controller get
+const loginControllerGet = (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
+};
+
+// login controller post
+const loginControllerPost = async (req, res) => {
+  try {
+  } catch (error) {}
+};
 
 module.exports = {
-   registerControllerGet,
+  registerControllerGet,
+  registerControllerPost,
+  loginControllerGet,
+  loginControllerPost,
 };
--- node_jwt/jwt/server/routes/user.routes.js
+++ node_jwt/jwt/server/routes/user.routes.js
@@ -1,36 +1,19 @@
 const router = require("express").Router();
 const path = require("path");
 const userModel = require("../model/user.model");
-const { registerControllerGet } = require("../controller/user.controller");
+const { registerControllerGet, registerControllerPost, loginControllerGet, loginControllerPost } = require("../controller/user.controller");
 
 // register route get
 router.get("/register", registerControllerGet);
 
 // register route post
-router.post("/register", (req, res) => {
-  // check user is already registered or not
-  const user = userModel.findOne({ email: req.body.email });
-  if (user){
-    return res.status(400).json({ message: "User already exists!" });
-  }
-  const newUser = new userModel({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  })
-  newUser.save()
-  res.status(201).redirect("/users/login");
-});
+router.post("/register", registerControllerPost);
 
 // login route get
-router.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
-});
+router.get("/login", loginControllerGet);
 
 
 // login route post
-router.post("/login", (req, res) => {
-    
-})
+router.post("/login", loginControllerPost)
 
 module.exports = router;
